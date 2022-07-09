a2fce797f064afcaac47fea99f1a5be0354482a3
PADM KESAV <80171563+Padm0069@users.noreply.github.com>
2022-07-09 22:49:23 +0530

    Time: 3 ms (58.46%), Space: 6 MB (39.78%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n < 2)
+            return n;
+        int memo[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i=2; i<=n; i++)
+            memo[i] = memo[i-1] + memo[i-2];
+        return memo[n];
+    }
+};
