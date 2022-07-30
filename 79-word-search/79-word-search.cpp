79ce3717591e6750f807afe53fe142cddbf3036b
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2022-07-30 12:30:09 +0530

    Time: 1567 ms (5.04%), Space: 8 MB (37.13%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
+    
+    //pass board by reference
+    bool DFS(vector<vector<char>>& board, string word, int i, int j, int n) {
+		//check if all the alphabets in the word is checked
+        if(n == word.size()) return true; 
+        
+		//check if i and j are out of bound or if the characters aren't equal
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) return false;
+        
+		//mark as visited 
+        board[i][j] = '0';
+        
+		//branch out in all 4 directions
+        bool status = DFS(board, word, i + 1, j, n + 1) ||  //down
+                        DFS(board, word, i, j + 1, n + 1) ||  //right
+                        DFS(board, word, i - 1, j, n + 1) ||  //up
+                        DFS(board, word, i, j - 1, n + 1);  //left
+        
+		//change the character back for other searches
+        board[i][j] = word[n];
+		
+        return status;
+    }
+    
+    
     bool exist(vector<vector<char>>& board, string word) {
-    for (unsigned int i = 0; i < board.size(); i++) 
-        for (unsigned int j = 0; j < board[0].size(); j++) 
-            if (dfs(board, i, j, word))
-                return true;
-    return false;
-}
-
-bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-    if (!word.size())
-        return true;
-    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+        if(word == "") return false;
+        
+        for(int i = 0; i < board.size(); i++) 
+            for(int j = 0; j < board[i].size(); j++) 
+				//check if the characters are equal then call DFS
+                if(board[i][j] == word[0] && DFS(board, word, i, j, 0))
+                    return true;
+        
         return false;
-    char c = board[i][j];
-    board[i][j] = '*';
-    string s = word.substr(1);
-    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-    board[i][j] = c;
-    return ret;
-}
+    }
 };
