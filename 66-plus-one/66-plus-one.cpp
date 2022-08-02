704bba5b05d858e8b25200292f7642dba0e50886
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2022-08-03 00:26:17 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (85.86%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--)
+    {
+        if (digits[i]++ < 9)
+            return digits;
+        digits[i] = 0;
+    }
+    digits[0] = 1;
+    digits.push_back(0);
+    return digits;
+    }
+};
