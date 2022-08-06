09e6944f3e0bdf917a33870ebeacf87f683c7524
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2022-08-06 16:42:37 +0530

    Time: 74 ms (81.21%), Space: 45 MB (50.33%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,37 +1,73 @@
+#define MAX_NODES 10000
 class Trie {
+    struct Trienode
+    {
+        char val;
+        int count;
+        int endsHere;
+        Trienode *child[26];
+    };
+    Trienode *root;
+    
+    Trienode *getNode(int index)
+    {
+        Trienode *newnode = new Trienode;
+        newnode->val = 'a'+index;
+        newnode->count = newnode->endsHere = 0;
+        for(int i=0;i<26;++i)
+            newnode->child[i] = NULL;
+        return newnode;
+    }
 public:
-    Trie() {}
-
+    /** Initialize your data structure here. */
+    Trie() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        root = getNode('/'-'a');
+    }
+    
+    /** Inserts a word into the trie. */
     void insert(string word) {
-        Trie* node = this;
-        for (char ch : word) {
-            if (!node->next.count(ch)) { node->next[ch] = new Trie(); }
-            node = node->next[ch];
+        Trienode *curr = root;
+        int index;
+        for(int i=0;word[i]!='\0';++i)
+        {
+            index = word[i]-'a';
+            if(curr->child[index]==NULL)
+                curr->child[index] = getNode(index);
+            curr->child[index]->count +=1;
+            curr = curr->child[index];
         }
-        node->isword = true;
+        curr->endsHere +=1;
     }
-
+    
+    /** Returns if the word is in the trie. */
     bool search(string word) {
-        Trie* node = this;
-        for (char ch : word) {
-            if (!node->next.count(ch)) { return false; }
-            node = node->next[ch];
+        Trienode *curr = root;
+        int index;
+        for(int i=0;word[i]!='\0';++i)
+        {
+            index = word[i]-'a';
+            if(curr->child[index]==NULL)
+                return false;
+            curr = curr->child[index];
         }
-        return node->isword;
+        return (curr->endsHere > 0);
     }
-
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        Trie* node = this;
-        for (char ch : prefix) {
-            if (!node->next.count(ch)) { return false; }
-            node = node->next[ch];
+        Trienode *curr = root;
+        int index;
+        for(int i=0;prefix[i]!='\0';++i)
+        {
+            index = prefix[i]-'a';
+            if(curr->child[index]==NULL)
+                return false;
+            curr = curr->child[index];
         }
-        return true;
+        return (curr->count > 0);
     }
-
-private:
-    map<char, Trie*> next = {};
-    bool isword = false;
 };
 /**
  * Your Trie object will be instantiated and called as such:
