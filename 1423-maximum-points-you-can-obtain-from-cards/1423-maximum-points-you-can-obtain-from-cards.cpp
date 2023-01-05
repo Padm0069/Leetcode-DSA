b2637686760763e737b430d209fc63fd298e6e08
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-01-05 21:04:26 +0530

    Time: 99 ms (53.13%), Space: 42.4 MB (49.93%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // 1 <= cardPoints.length <= 10**5
+    int maxScore(vector<int>& cardPoints, int k) {
+        int res = 0;
+		
+		//First k elements in our window
+        for(int i=0;i<k;i++) res+=cardPoints[i];
+        
+        int curr=res;
+        for(int i=k-1;i>=0;i--) {  //after hitting window of size k
+			//We remove the last visited element and add the non-visited element from the last
+            curr-=cardPoints[i];
+            curr+=cardPoints[cardPoints.size()-k+i];
+			
+            //We check the maximum value any possible combination can give
+			res = max(res, curr);
+        }
+        
+        return res;
+    }
+};
