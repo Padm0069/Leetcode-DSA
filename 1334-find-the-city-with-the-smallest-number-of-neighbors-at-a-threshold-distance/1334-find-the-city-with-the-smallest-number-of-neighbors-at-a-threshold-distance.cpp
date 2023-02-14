f94c60b4b51b48fcd7d640b9ed7285543ecba2b3
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-02-15 01:05:17 +0530

    Time: 87 ms (83.45%), Space: 12.1 MB (83.10%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -4,7 +4,7 @@ public:
         
         //using floyd warshall
         vector<vector<int>> dis(n, vector(n, 10001));
-        int res = 0, smallest = n;
+        int res = 0, smallest = n;   //max possible city possible is n
         for (auto& e : edges)
             dis[e[0]][e[1]] = dis[e[1]][e[0]] = e[2];
         for (int i = 0; i < n; ++i)
@@ -16,9 +16,9 @@ public:
         for (int i = 0; i < n; i++) {
             int count = 0;
             for (int j = 0; j < n; ++j)
-                if (dis[i][j] <= distanceThreshold)
-                    ++count;
-            if (count <= smallest) {
+                if (dis[i][j] <= distanceThreshold)     //distance less than the threshold
+                    ++count;                            //count for every city(node)
+            if (count <= smallest) {                    //returning the one having the lowest count.
                 res = i;
                 smallest = count;
             }
