3d8b02492b03a02efb0b21391fdc9d742033ef14
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-02-27 14:50:34 +0530

    Time: 905 ms (43.26%), Space: 229.7 MB (42.01%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
@@ -1,58 +1,71 @@
+struct Node{
+    Node *links[2];
+    bool contain(int bit){
+        return links[bit]!=NULL;
+    }
+    void put(int bit,Node *node){
+        links[bit]=node;
+    }
+    Node *get(int bit){
+        return links[bit];
+    }
+};
+class Trie{
+    private: Node *root;
+    public:
+    Trie(){
+        root=new Node();
+    }
+    void insert(int &num){
+        Node *node=root;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(!node->contain(bit)){
+                node->put(bit,new Node());
+            }node=node->get(bit);
+        }
+    }
+    int getMax(int &num){
+        Node *node=root;
+        int maxi=0;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(node->contain(1-bit)){
+                maxi=maxi|(1<<i);
+                node=node->get(1-bit);
+            }
+            else node=node->get(bit);
+        }
+        return maxi;
+    }
+};
 class Solution {
-    class TreeNode{
-        public : 
-          TreeNode * next[2];
-          int storeVal;
-          TreeNode() {next[0] = NULL, next[1] = NULL, storeVal = 0;}
-    };
-    void insert(TreeNode *root, int val)
-    {
-        for(int i=31;i>=0;i--)
-        {
-            bool res= (val & (1 << i));
-            if(root->next[res] == NULL) 
-               root->next[res]=new TreeNode();
-            root=root->next[res];
-        }
-        root->storeVal = val;
-    }
-    int search(TreeNode *root, int val)
-    {
-        for(int i = 31; i >= 0; i--)
-        {
-            bool res = (val & (1 << i));
-            if(root->next[1 - res] != NULL)
-                root = root->next[1 - res];
-            else
-                root=root->next[res];
-        }
-       return (root->storeVal ^ val); 
-    }
 public:
-    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
-          int n = nums.size();
-          // To get the correct order after sorting
-          for(int i = 0; i < queries.size() ; i++)
-              queries[i].push_back(i);
-          // Sort the queries according to accending mi values     
-          sort(queries.begin(),queries.end(), [](auto &a, auto &b){ return a[1] < b[1];});
-          sort(nums.begin(), nums.end());
-          int index = 0;
-          TreeNode * root = new TreeNode();
-          
-          vector<int>res(queries.size(),-1);
-          for(auto q : queries)
-          {
-              //keep inserting all the values which are lesser than mi in the trie
-              while(index < n && nums[index] <= q[1])
-              {
-                  insert(root, nums[index]);
-                  index ++;
-              }
-              // if any value inserted then we are now in a state of taking xor with atleast any number which satifies the limit                  condition
-              if(index)
-                res[q[2]] =  search(root, q[0]); 
-          }
-        return res;
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& qu) {
+        Trie trie;
+        int i;
+        vector<vector<int>>arr;
+        for(i=0;i<qu.size();i++){
+            int x=qu[i][0];
+            int lim=qu[i][1];
+            arr.push_back({lim,x,i});
+        }
+        vector<int>ans(qu.size());
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        sort(arr.begin(),arr.end());
+        int ind=0;
+        for(i=0;i<qu.size();i++){
+            int lim=arr[i][0];
+            int x=arr[i][1];
+            int qind=arr[i][2];
+            while(ind<n&&nums[ind]<=lim){
+                trie.insert(nums[ind]);
+                ind++;
+            }
+            if(ind==0) ans[qind]=-1;
+            else ans[qind]=trie.getMax(x);
+        }
+        return ans;
     }
 };
