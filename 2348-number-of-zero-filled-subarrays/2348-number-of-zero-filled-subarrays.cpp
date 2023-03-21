787d31fc0f42a4378a9f8f6d0f40708689074c82
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-03-21 23:54:29 +0530

    Time: 169 ms (72.85%), Space: 107.5 MB (54.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans = 0;
-        long long count = 0;
-        for(auto i:nums){
-            if(i){
-              ans += (count*(count+1))/2;
-              count = 0;
-            }else count++;
+        long long ans = 0;             // for storing the final answer (Total no. of subarrays)
+        long long cnt = 0;             // for counting the consecutive 0's
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            // wherever we find 0 just increase cnt by 1 and sum up to ans
+            if(nums[i]==0)               
+            {
+                cnt++;
+                ans += cnt;
+            }
+            
+            // if it is none 0 them make 0 count 0
+            else
+            {
+                cnt = 0;
+            }
         }
-        ans+= (count*(count+1))/2;
         return ans;
     }
+    
 };
