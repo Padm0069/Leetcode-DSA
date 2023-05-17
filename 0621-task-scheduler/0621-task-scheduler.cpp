919fa2ee15e113b53f4279c2eb49bccb15caa22a
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-05-18 00:42:18 +0530

    Time: 61 ms (91.29%), Space: 34.5 MB (44.69%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-         int m[128] = {0};
-        for (char c : tasks)  m[c]++;  
-        sort(m, m + 128, [](int a, int b){ return a > b; });
-        int gap = n * (m[0] - 1), total = m[0] + gap;
-        for (int i=1; i<128 && m[i] != 0; i++) {
-            if (gap >= m[i]) {
-                if (m[i] == m[0]) {
-                    gap -= (m[0] - 1);      
-                    total++;
-                } else {
-                    gap -= m[i];
-                }
-            } else {
-                return tasks.size();
-            }
-        } 
-        return total;
+        vector<int>hash(26,0);
+        for(char c:tasks)hash[c-'A']++;
+        sort(hash.begin(),hash.end(),greater<int>());
+        int slots=hash[0]-1;
+        int idle=slots*n;
+        for(int i=1;i<hash.size();i++)     //filling up the idle slot
+        {
+            idle-=min(slots,hash[i]);
+        }
+        return tasks.size()+max(0,idle);
     }
 };
 
