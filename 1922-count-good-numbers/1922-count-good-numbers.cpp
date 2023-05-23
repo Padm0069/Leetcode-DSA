6807dcbb25852838a26c613411f81c763926fc91
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-05-24 00:01:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (69.32%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -32,24 +32,3 @@ public:
       return (int)ans;
     }
 };
-
-
-// Now, we know we have 4 primes = {2, 3, 5, 7} and 5 evens = {0, 2, 4, 6, 8}
-
-// if index == 0
-// then there can be any of one evens at even position, so 5 ways
-// ans = 5
-
-// if index == 1
-// then there was 1 even at index = 0, and at this odd index there can be one of 4 primes
-// ans = 5* 4(this pos)
-
-// if index == 2
-// then at this even index there can be one of 5 evens
-// ans = (5*4) * 5(this pos)
-
-// so, continuing the pattern we can see, it's like, 5*4*5*4*5*4*5..... ans so on
-// here no. of 4s = no. of odd positions = n/2
-// no. of 5s = no. of even positions = (n-n/2)
-
-// ans = pow(4,count4) * pow(5,count5)
