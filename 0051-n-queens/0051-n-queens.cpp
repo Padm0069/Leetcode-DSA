c2868d07ca86d3e9998feb1d8d0b4c0468b23aa2
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-05-24 17:44:24 +0530

    Time: 9 ms (50.59%), Space: 7.5 MB (49.11%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-     void solve(int col, vector < string > & board, vector < vector < string >> & ans, vector < int > & leftrow, vector < int > & upperDiagonal, vector < int > & lowerDiagonal, int n) {
-      if (col == n) {
-        ans.push_back(board);
-        return;
-      }
-      for (int row = 0; row < n; row++) {
-        if (leftrow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
-          board[row][col] = 'Q';
-          leftrow[row] = 1;
-          lowerDiagonal[row + col] = 1;
-          upperDiagonal[n - 1 + col - row] = 1;
-          solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
-          board[row][col] = '.';
-          leftrow[row] = 0;
-          lowerDiagonal[row + col] = 0;
-          upperDiagonal[n - 1 + col - row] = 0;
-        }
-      }
+     vector<vector<string> > sols; // 2D vector of strings to store the solutions
+    vector<vector<string>> solveNQueens(int n) {    
+	    vector<string> board(n, string(n, '.'));  // creating an empty board
+	    solve(board, 0); // calling the recursive function
+	    return sols; 
     }
-    vector<vector<string>> solveNQueens(int n) {
-        vector < vector < string >> ans;
-      vector < string > board(n);
-      string s(n, '.');
-      for (int i = 0; i < n; i++) {
-        board[i] = s;
-      }
-      vector < int > leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
-      solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
-      return ans;
+    
+    bool isSafe(vector<string>& board, int row, int col) {
+	    int n = size(board);
+	    for(int i = 0; i < n; i++) {
+			// checking if there is a queen in the same column
+		    if(board[i][col] == 'Q') return false; 
+		    // checking if there is a queen in the same diagonal (left to right)
+		    if(row - i >= 0 && col - i >= 0 && board[row - i][col - i] == 'Q') return false;
+		    if(row - i >= 0 && col + i <  n && board[row - i][col + i] == 'Q') return false;
+		    // No need to traverse more since the lower rows below current row attribute will always be safe.
+			/* if(row + i <  n && col - i >= 0 && board[row + i][col - i] == 'Q') return false;
+			if(row + i <  n && col + i <  n && board[row + i][col + i] == 'Q') return false; */
+	    }
+	    return true;
+    }
+    
+    // Recursive Function (solve) - It basically tries all possible placement of queen for the current row & recurses for it's next row
+    void solve(vector<string>& board, int row) {
+	// Base condition. 
+    // We reached the last row, so we have a solution so we add it to the solution vector
+	    if(row == size(board)) { 
+		    sols.push_back(board);
+		    return;
+    	}            
+	    // Try placing a queen on each column for a given row. 
+        // Explore next row by placing Q at each valid column for the current row
+	    for(int col = 0; col < size(board); col++){
+		    if(isSafe(board, row, col)) {
+			    board[row][col] = 'Q';    // Queen placed on a valid cell
+			    solve(board, row + 1);    // Exploring next row
+			    board[row][col] = '.';    // Backtracking to get all possible solutions
+		    }
+        }
     }
 };
