d9c7ede05b1aabea61926e622e6a4127bb6e4376
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-05-29 01:14:28 +0530

    Time: 1609 ms (8.02%), Space: 292.4 MB (7.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    // There's a typical DP solution with O(N^2) Time and O(N) space 
-    // DP[i] means the result ends at i
-    // So for dp[i], dp[i] is max(dp[j]+1), for all j < i and nums[j] < nums[i]
-    int lengthOfLIS(vector<int>& nums) {
-        const int size = nums.size();
-        if (size == 0) { return 0; } 
-        vector<int> dp(size, 1);
-        int res = 1;
-        for (int i = 1; i < size; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j]+1);
-                }
-            }
-            res = max (res, dp[i]);
+    int f(int i,int prev, vector<int>& nums, int n,vector<vector<int>>&dp){
+        if(i==n)return 0;
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+        int len=0+f(i+1,prev,nums,n,dp);
+        if(prev==-1||nums[i]>nums[prev]){
+            len=max(len,1+f(i+1,i,nums,n,dp));
         }
-        return res;
+        return dp[i][prev+1]=len;
     }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return f(0,-1,nums,n,dp);
+        
+    }
+    
 };
