ccfd003daeb51dad03074211d37884440bd84792
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-06-22 02:33:09 +0530

    Time: 8 ms (74.30%), Space: 11.4 MB (35.16%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,48 +19,37 @@ public:
     Node* copyRandomList(Node* head) {
        
 
-          Node *iter = head; 
-          Node *front = head;
-
-          // First round: make copy of each node,
-          // and link them together side-by-side in a single list.
-          while (iter != NULL) {
-            front = iter->next;
-
-            Node *copy = new Node(iter->val);
-            iter->next = copy;
-            copy->next = front;
-
-            iter = front;
-          }
-
-          // Second round: assign random pointers for the copy nodes.
-          iter = head;
-          while (iter != NULL) {
-            if (iter->random != NULL) {
-              iter->next->random = iter->random->next;
-            }
-            iter = iter->next->next;
-          }
-
-          // Third round: restore the original list, and extract the copy list.
-          iter = head;
-          Node *pseudoHead = new Node(0);
-          Node *copy = pseudoHead;
-
-          while (iter != NULL) {
-            front = iter->next->next;
-
-            // extract the copy
-            copy->next = iter->next;
-
-            // restore the original list
-            iter->next = front;
-              
-            copy = copy -> next; 
-            iter = front;
-          }
-
-          return pseudoHead->next;
+         // this map points to the clone of the original node
+        unordered_map<Node*,Node*> mp;
+        
+        Node* temp=head;
+        
+        // 1st Pass - Creation of Nodes and no wiring
+        while(temp!=NULL)
+        {
+            // Pointing the original node to its clone node
+            // Now I can access the clone node in O(1)
+            mp[temp] = new Node(temp->val);
+            temp=temp->next;
+            
+        }
+        
+        // 2nd pass - Filling the next and random pointers
+        temp=head;
+        
+        while(temp!=NULL)
+        {
+            // mp[ptr] is the clone node and its next is the next original
+            // node's clone node. 
+            // mp[ptr->next] refers to the clone node of ptr->next;
+            mp[temp]->next= mp[temp->next]; 
+            
+            // similar thing for the random pointer
+            mp[temp]->random=mp[temp->random];
+            
+            temp=temp->next;
+        }
+        
+        return mp[head];
     }
 };
