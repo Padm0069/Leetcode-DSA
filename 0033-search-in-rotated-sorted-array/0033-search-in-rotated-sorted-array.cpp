b7a492f6dd7f780d6bfbd7e60d60b56fc9c433fb
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-07-09 18:56:15 +0530

    Time: 5 ms (46.85%), Space: 10.9 MB (99.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        //A.V BINARY SEARCH VIDEO NO - 8
-        
         int n = nums.size();
-        int lo=0,hi=n-1;
-        // find the index of the smallest value using binary search.
-        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
-        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-        while(lo<hi){
-            int mid=(lo+hi)/2;
-            if(nums[mid]>nums[hi]) lo=mid+1;
-            else hi=mid;
-        }
-        // lo==hi is the index of the smallest value and also the number of places rotated.
-        int rot=lo;
-        lo=0;hi=n-1;
-        // The usual binary search and accounting for rotation.
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int realmid=(mid+rot)%n;  //accounting for rotation
-            if(nums[realmid]==target)return realmid;
-            if(nums[realmid]<target)lo=mid+1;
-            else hi=mid-1;
+        int low = 0, high = n-1, mid;
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            if(nums[mid] == target) return mid;
+            //Now we look for which half is sorted in the array 
+            if(nums[mid]>=nums[low])
+            {
+                if(nums[low]<=target  && target <= nums[mid])
+                {
+                    high = mid-1;
+                }
+                else 
+                    low = mid + 1;
+            }
+            else
+            {
+                if(target<=nums[high] && target >= nums[mid])
+                {
+                    low = mid+1;
+                }
+                else high = mid-1;
+            }
         }
         return -1;
     }
