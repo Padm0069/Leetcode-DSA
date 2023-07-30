8c16cf5ccdd2819e1dfb1b6d5b0a608c642612e2
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-07-30 16:36:06 +0530

    Added solution - LeetHub

---
+++ Set the rightmost unset bit - GFG/set-the-rightmost-unset-bit.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int setBit(int N)
+    {
+        // Write Your Code here
+        
+        return N & (N+1)? N|(N+1):N;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans = ob.setBit(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
