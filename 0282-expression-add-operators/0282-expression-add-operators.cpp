2c3a61ae91d1512d80624993cc779d12f37c932b
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-08-29 19:00:32 +0530

    Time: 670 ms (13.94%), Space: 124.5 MB (29.97%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,33 +1,29 @@
-class Solution {
-public:
-    void generate(string &num, int target, int curr, vector<string> &res, string build, long long int prevNumber, long long int currSum){
-        if(curr==num.size()){
-            if(currSum==target){
-                res.push_back(build);
-            }
-            return;
-        }
-        for(int len=1;len<=(num.size()-curr);len++){
-            string number=num.substr(curr,len);
-            if(number[0]=='0' && number.size()>1){
-                continue;
-            }
-            long long int currNum=stoll(number);
-            generate(num,target,curr+len,res,build+"+"+number,currNum,currSum+currNum);
-            generate(num,target,curr+len,res,build+"-"+number,-currNum,currSum-currNum);
-            generate(num,target,curr+len,res,build+"*"+number,currNum*prevNumber,currSum-prevNumber+currNum*prevNumber);
-        }
-    }
-    vector<string> addOperators(string num, int target) {
-        vector<string> res;
-        for(int len=1;len<=num.size();len++){
-            string number=num.substr(0,len);
-            if(number[0]=='0' && number.size()>1){
-                continue;
-            }
-            long long int currNum=stoll(number);
-            generate(num,target,len,res,number,currNum,currNum);
-        }
-        return res;
-    }
+class Solution{
+    public:
+    void dfs(string &num, int start, int target, string path, long prev, long cur, vector<string> &res) {
+	if(start == num.size() && prev + cur == target) res.push_back(path);
+
+	for(int i = 1; start + i <= num.size(); i++) {
+		string s = num.substr(start, i);
+		long n = stoll(s);
+		if(to_string(n).size() != s.size()) return;
+		if(!start) dfs(num, i, target, s, 0, n, res);
+		else {
+			dfs(num, start + i, target, path + "+" + s, prev + cur, n, res);
+			dfs(num, start + i, target, path + "-" + s, prev + cur, -n, res);
+			dfs(num, start + i, target, path + "*" + s, prev, cur * n, res);
+		}
+	}
+}
+
+vector<string> addOperators(string num, int target) {
+	vector<string> res;
+	dfs(num, 0, target, "", 0, 0, res);
+	return res;
+}
+
+    
 };
+
+
+
