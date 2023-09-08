4c2f7d8234e427dad5ecf5384ddb1cd31c02d56c
Padm Keshav <80171563+Padm0069@users.noreply.github.com>
2023-09-08 15:40:18 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (49.34%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,8 +4,8 @@ public:
         vector<vector<int>> r(numRows);
 
         for (int i = 0; i < numRows; i++) {
-            r[i].resize(i + 1);
-            r[i][0] = r[i][i] = 1;
+            r[i].resize(i + 1);     //dynamic array allocation
+            r[i][0] = r[i][i] = 1;      //first and last one
   
             for (int j = 1; j < i; j++)
                 r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
